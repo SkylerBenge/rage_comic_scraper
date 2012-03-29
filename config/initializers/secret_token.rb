16106c53811ff7249ce89c18a3c26ddb7da2a7bb
Adam Darrah <piremies@gmail.com>
2012-03-29 12:46:09 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rage::Application.config.secret_token = 'ea9ac965b26e0e0cf105c0c1d423f2886b36f4210da4a40724105bec9bd7166af31da910b1264d8dceec7ea9303f92420ed06d24c7e688df162ee3bec138bbe5'
